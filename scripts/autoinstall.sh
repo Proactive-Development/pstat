8f6e139f3895bfad859d1f23862260d9e178afe8
WolfieBoyy <joshuaxmas@icloud.com>
2022-03-20 18:52:01 +0000

    Fix Install Script

--- scripts/autoinstall.sh
+++ scripts/autoinstall.sh
@@ -1,1 +1,1 @@
-sudo curl https://github.com/Proactive-Development/pstat/releases/download/latest/pstat -o /usr/local/bin/pstat && sudo chmod +x /usr/bin/pstat
+sudo curl https://github.com/Proactive-Development/pstat/releases/download/0.1.2/pstat -o /usr/local/bin/pstat && sudo chmod +x /usr/local/bin/pstat
