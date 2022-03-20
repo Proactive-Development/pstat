32e00dc856f20c79b7666d1f3947e2e82b4b20da
WolfieBoy <joshuaxmas@icloud.com>
2022-03-20 20:04:35 +0000

    Update autoinstall.sh

--- scripts/autoinstall.sh
+++ scripts/autoinstall.sh
@@ -2,4 +2,4 @@ sudo apt update
 sudo apt install python3 python3-pip
 pip install psutil
 
-sudo curl https://github.com/Proactive-Development/pstat/releases/download/0.1.2/pstat -o /usr/local/bin/pstat && sudo chmod +x /usr/local/bin/pstat
+sudo curl https://github.com/Proactive-Development/pstat/releases/download/0.1.2/pstat -o /usr/bin/pstat && sudo chmod +x /usr/bin/pstat
