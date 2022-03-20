cdd1c66e875986717f94f6acc907c6fad8846899
awesomelewis2007 <awesomelewis2007@gmail.com>
2022-03-20 17:20:29 +0000

    Clean up code

--- src/__main__.py
+++ src/__main__.py
@@ -31,61 +31,73 @@ if __name__ == "__main__":
         if arg == "-V" or arg == "--app-version":
             print(VERSION)
             exit()
+
         elif arg == "-i" or arg == "--cpuinfo":
             with open("/proc/cpuinfo","r") as f:
                 print(f.read())
                 f.close()
             exit()
+
         elif arg == "-s" or arg == "--stat":
             with open("/proc/stat","r") as f:
                 print(f.read())
                 f.close()
             exit()
+
         elif arg == "-I" or arg == "--interrupts":
             with open("/proc/interrupts","r") as f:
                 print(f.read())
                 f.close()
             exit()
+
         elif arg == "-f" or arg == "--filesystems":
             with open("/proc/filesystems","r") as f:
                 print(f.read())
                 f.close()
             exit()
+
         elif arg == "-m" or arg == "--iomem":
             with open("/proc/iomem","r") as f:
                 print(f.read())
                 f.close()
             exit()
+
         elif arg == "-M" or arg == "--mounts":
             with open("/proc/mounts","r") as f:
                 print(f.read())
                 f.close()
             exit()
+
         elif arg == "-p" or arg == "--partitions":
             with open("/proc/partitions","r") as f:
                 print(f.read())
                 f.close()
             exit()
+
         elif arg == "-u" or arg == "--uptime":
             with open("/proc/uptime","r") as f:
                 print(f.read())
                 f.close()
             exit()
+
         elif arg == "-v" or arg == "--version":
             with open("/proc/version","r") as f:
                 print(f.read())
                 f.close()
             exit()
+
         elif arg == "-w" or arg == "--wireless":
             with open("/proc/net/wireless","r") as f:
                 print(f.read())
                 f.close()
             exit()
+
         elif arg == "-mi" or arg == "--memory-info":
             with open("/proc/meminfo","r") as f:
                 print(f.read())
                 f.close()
             exit()
+
         elif arg == "-lp" or arg == "--ls-pids":
             for i in os.listdir("/proc"):
                 if i.replace("_","").replace("-","").isalpha() == False:
@@ -109,9 +121,10 @@ if __name__ == "__main__":
             except PermissionError:
                 print("Error reading the io file of "+sys.argv[sys.argv.index(arg)+1]+" try running with sudo")
             exit()
+
         elif arg == "-h" or arg == "--help":
             print(help_msg)
             exit()
 
-    print("Invalid/Unknown Command")
+    print("Invalid/Unknown Command for help run -h or --help")
     print(commands)
