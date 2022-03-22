f9c12bb055cc143660db942a84f8bfcf0e5487be
WolfieBoyy <joshuaxmas@icloud.com>
2022-03-22 19:02:11 +0000

    Update autoinstall

--- scripts/autoinstall.sh
+++ scripts/autoinstall.sh
@@ -1,22 +1,32 @@
-echo "Welcome to the pstat autoinstaller."
-echo "This script will install pstat on your system."
-echo "Use control-c to cancel."
-echo "The installer will ask for your sudo password."
-echo "This will install pstat in /usr/local/bin."
-echo "This will also install python3 and pip3 as well as the following dependencies:"
-echo "    - psutil."
-read -p "Are you alright? (y/n) " RESP
-if [ "$RESP" = "y" ]; then
-  echo "Installin pstat..."
-  sudo apt update
-  sudo apt install python3 python3-pip
-  pip install psutil
-  sudo curl -fsSL https://github.com/Proactive-Development/pstat/releases/download/latest/pstat -o /usr/bin/pstat && sudo chmod +x /usr/bin/pstat
-else
-  echo "You need more bash programming"
+NOCOLOR='\033[0m'
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHTGRAY='\033[0;37m'
+DARKGRAY='\033[1;30m'
+LIGHTRED='\033[1;31m'
+LIGHTGREEN='\033[1;32m'
+YELLOW='\033[1;33m'
+LIGHTBLUE='\033[1;34m'
+LIGHTPURPLE='\033[1;35m'
+LIGHTCYAN='\033[1;36m'
+WHITE='\033[1;37m'
+
+echo "Welcome to the pstat auto install script"
+read -p "Do you want to install pstat and the requierd dependancies [Y/N]> " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    echo "========================================"
+    sudo apt update
+    sudo apt install python3 python3-pip
+    pip install psutil
+
+    sudo curl -fsSL https://github.com/Proactive-Development/pstat/releases/download/latest/pstat -o /usr/bin/pstat && sudo chmod +x /usr/bin/pstat
+    echo "========================================"
+    echo -e "${GREEN}Pstat is now installed on your system ${NOCOLOR}"
+    echo -e "${NOCOLOR}If you need any${BLUE} help ${NOCOLOR}"
 fi
-
-done
-
-
-
