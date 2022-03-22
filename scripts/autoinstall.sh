81e16968c14d8a9a39e33c72a129010ad27adff9
WolfieBoyy <joshuaxmas@icloud.com>
2022-03-22 18:58:37 +0000

    Update script

--- scripts/autoinstall.sh
+++ scripts/autoinstall.sh
@@ -3,16 +3,19 @@ echo "This script will install pstat on your system."
 echo "Use control-c to cancel."
 echo "The installer will ask for your sudo password."
 echo "This will install pstat in /usr/local/bin."
-echo "This will aslo install python3 and pip3. as well as the following dependencies:"
+echo "This will also install python3 and pip3 as well as the following dependencies:"
 echo "    - psutil."
-select yn in "Yes" "No"; do
-    case $yn in
-        Yes ) sudo apt update
-              sudo apt install python3 python3-pip
-              pip install psutil
-              sudo curl -fsSL https://github.com/Proactive-Development/pstat/releases/download/latest/pstat -o /usr/bin/pstat && sudo chmod +x /usr/bin/pstat
-        No ) exit;;
-    esac
+read -p "Are you alright? (y/n) " RESP
+if [ "$RESP" = "y" ]; then
+  echo "Installin pstat..."
+  sudo apt update
+  sudo apt install python3 python3-pip
+  pip install psutil
+  sudo curl -fsSL https://github.com/Proactive-Development/pstat/releases/download/latest/pstat -o /usr/bin/pstat && sudo chmod +x /usr/bin/pstat
+else
+  echo "You need more bash programming"
+fi
+
 done
 
 
