59e4fde2918100d0fe00b7d406cf0aec3983c1c9
WolfieBoyy <joshuaxmas@icloud.com>
2022-03-23 18:20:42 +0000

    Edit

--- scripts/testinstall.sh
+++ scripts/testinstall.sh
@@ -1,8 +1,41 @@
-while true; do
-    read -p "Do you wish to install this program?" yn
-    case $yn in
-        [Yy]* ) make install; break;;
-        [Nn]* ) exit;;
-        * ) echo "Please answer yes or no.";;
+confirm() {
+#
+# syntax: confirm [<prompt>]
+#
+# Prompts the user to enter Yes or No and returns 0/1.
+#
+# This  program  is free software: you can redistribute it and/or modify  it
+# under the terms of the GNU General Public License as published by the Free
+# Software  Foundation, either version 3 of the License, or (at your option)
+# any later version.
+#
+# This  program  is  distributed  in the hope that it will  be  useful,  but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public  License
+# for more details.
+#
+# You  should  have received a copy of the GNU General Public License  along
+# with this program. If not, see <http://www.gnu.org/licenses/>
+#
+#  04 Jul 17   0.1   - Initial version - MEJT
+#
+  local _prompt _default _response
+ 
+  if [ "$1" ]; then _prompt="$1"; else _prompt="Are you sure"; fi
+  _prompt="$_prompt [y/n] ?"
+ 
+  # Loop forever until the user enters a valid response (Y/N or Yes/No).
+  while true; do
+    read -r -p "$_prompt " _response
+    case "$_response" in
+      [Yy][Ee][Ss]|[Yy]) # Yes or Y (case-insensitive).
+        return 0
+        ;;
+      [Nn][Oo]|[Nn])  # No or N.
+        return 1
+        ;;
+      *) # Anything else (including a blank) is invalid.
+        ;;
     esac
-done
+  done
+}
