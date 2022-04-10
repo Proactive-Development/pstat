166c45d68b54540e2454a665fcba2e9de7dae263
WolfieBoy <joshuaxmas@icloud.com>
2022-04-10 12:25:37 +0100

    Create betainstall.sh

---
+++ scripts/betainstall.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "========================================"
+sudo apt update
+sudo apt install python3 python3-pip
+pip install psutil
+
+sudo curl -fsSL https://github.com/Proactive-Development/pstat/releases/download/latest/pstat -o /usr/bin/pstat && sudo chmod +x /usr/bin/pstat
+echo "========================================"
